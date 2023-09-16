cb5a06086e448cb860fef43476fc62c0b8482608
xiaorui xie <144289219+xiaoruixie@users.noreply.github.com>
2023-09-16 16:34:28 -0400

    Add files via upload
    Completes-Lab-2

---
+++ Completes-Lab-2/js/script.js
@@ -0,0 +1,19 @@
+k = 88;
+let nulVal = null;
+let undef;
+
+
+document.querySelectorAll("h1")[0].style.fontSize="8vw";
+document.querySelectorAll("h1")[0].textContent="hello there";
+var firstName= "Xiaorui";
+let x = 9;
+const pi = 3.14;
+x = 18 ;
+let favNumber = 8;
+let a = 2,b = 2;
+let sum = a + b;
+console .log('My name is '+ firstName +' .');
+let squared=favNumber**2;
+console .log('The squared of '+ favNumber + ' is ' + squared+' .');
+const product =`The squared of favNumber is aquared`;
+
